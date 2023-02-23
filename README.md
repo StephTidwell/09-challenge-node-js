6a227bb52d5130143648c73ddf306cc7cfb894f8
Stephanie Tidwell <Stephanie@Corys-MacBook-Pro.local>
2023-02-23 17:37:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# test
+  ![License Badge](https://img.shields.io/static/v1?label=License&message=MIT&color=blue)
+  
+  Name: undefined
+  GitHub user name: undefined
+    
+  ## Table of contents:  
+  * [description](#description)
+  * [Installation](#Installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  
+  ## Description:
+  test test
+  ## Installation:
+  1, 2 and 3
+  ## Usage:
+  undefined
+  ## license:
+  MIT
+  
+  ## Contributing:
+  undefined
+  ## Testing:
+  undefined
+  ## Questions:
+  Contact undefined with any questions at undefined
---
+++ index.js
@@ -0,0 +1,122 @@
+// TODO: Include packages needed for this application
+const { prompt } = require("inquirer");
+const fs = require("fs");
+const path = require("path"); //works with fs, path is telling where files are or should go
+const generateMarkDown = require("./utils/generateMarkdown");
+
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("Please enter a title for your project to continue");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Briefly describe this project",
+    validate: (descriptionInput) => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log("Please enter a description for your project to continue");
+      }
+      return false;
+    },
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "List the steps required to install your project",
+    validate: (installationInput) => {
+      if (installationInput) {
+        return true;
+      } else {
+        console.log("Please enter installation info to continue");
+      }
+      return false;
+    },
+  },
+  {
+    type: "input",
+    name: "use",
+    message: "What is the useage of this application?",
+    validate: (useInput) => {
+      if (useInput) {
+        return true;
+      } else {
+        console.log("Please enter usage to continue");
+      }
+      return false;
+    },
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "Were there any contributors to your project?",
+    validate: (creditsInput) => {
+      if (creditsInput) {
+        return true;
+      } else {
+        console.log("Please enter credits to continue");
+      }
+      return false;
+    },
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    nmessage: "Is there a License you used?",
+    choices: ["MIT", "APACHE 2.0", "GPO 3.0", "none"],
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("Please enter email to continue");
+      }
+      return false;
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your github username",
+    validate: (githubInput) => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log("Please enter github to continue");
+      }
+      return false;
+    },
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  prompt(questions).then((responses) => {
+    console.log(responses);
+    console.log("generating readme...");
+    writeToFile("README.md", generateMarkDown({ ...responses })); //.. turns to ittrable array
+  });
+}
+
+// Function call to initialize app
+init();
